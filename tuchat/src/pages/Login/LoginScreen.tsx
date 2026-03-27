bd167f7e4cdf23abc96406fb1c86c900aff42efb
JacintoClubDeFans <noquieroserdelegado@gmail.com>
2026-03-27 16:00:17 +0000

    style: login 9

--- tuchat/src/pages/Login/LoginScreen.tsx
+++ tuchat/src/pages/Login/LoginScreen.tsx
@@ -49,7 +49,7 @@ const EyeSlashIcon = () => (
 
 const buildTileShadow = () => {
   const parts: string[] = [];
-  const gap = 36;
+  const gap = 18;
   const coef = -4.5;
 
   for (let i = 1; i <= 4; i += 1) {
